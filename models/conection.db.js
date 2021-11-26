25c3ab0cbcd9111c8438fb548a2e4d56b6c7c88d
TaseenAnsari <taseensaif@gmal.com>
2021-11-26 18:22:06 +0530

    done1

--- models/conection.db.js
+++ models/conection.db.js
@@ -1,11 +1,11 @@
 
 const Fawn = require("fawn");
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://taseenansari:tensea98351@cluster0.2o2yq.mongodb.net/test')
+mongoose.connect('mongodb+srv://taseenansari:"tensea98351"@cluster0.2o2yq.mongodb.net/test')
 .then(()=>console.log("connect to mongodb"))
 .catch((err)=>console.error(err.message));
 
-Fawn.init('mongodb+srv://taseenansari:tensea98351@cluster0.2o2yq.mongodb.net/test')
+Fawn.init('mongodb+srv://taseenansari:"tensea98351"@cluster0.2o2yq.mongodb.net/test')
 
 
 
